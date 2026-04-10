5bc187a2bdb046671aaf38f1b937bfcecf07f6ca
andreavaldez4 <valdezandrea2006@gmail.com>
2026-04-10 17:12:28 -0600

    feat: cambio de title y favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Skill Swap - Intercambia habilidades, crece junto a otros",
+  title: "Skill Swap",
   description:
     "Plataforma comunitaria para intercambiar habilidades sin dinero",
 };
